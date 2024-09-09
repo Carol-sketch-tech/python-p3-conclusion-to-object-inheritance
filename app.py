0efad13fcaa7383e2b0a5bdc7175ea4b0cbbc4ce
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-09 12:51:05 +0300

    conclusion on inheritance

---
+++ app.py
@@ -0,0 +1,10 @@
+'''
+Inheritance is a common concept in our everyday lives and over the last several lessons, we saw how python implements it.
+by crating a family of classes with shared behaviour while still differentiating those classes.
+becasue of inheritnace we can define basic pyhon classes with large reusability along with smaller subclasses for moer detailed bahavours.
+we can also take advantage of inheritance bycoding multiple non-repetitive methods that cut down on the total amount of code
+needed while also cleaning and optimizing the code we do need.
+we also looked at how we can use the super() function 
+to inherit from and extend methods in the parent class.
+finally we also looked at how we can use the super( function to inherit extended mehtods in the parent class.
+'''
